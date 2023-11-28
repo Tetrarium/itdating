e2ec3934ee89cee33aa5a1d4221676c6d594fd3c
Alexandr Turov <alexandr985crv@gmail.com>
2023-11-28 10:20:12 +0700

    init project add dependences

---
+++ webpack.prod.js
@@ -0,0 +1,14 @@
+const { merge } = require('webpack-merge');
+const TerserPlugin = require('terser-webpack-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const common = require('./webpack.common');
+
+module.exports = merge(common, {
+  mode: 'production',
+  optimization: {
+    minimizer: [
+      new TerserPlugin({}),
+      new CssMinimizerPlugin({}),
+    ],
+  },
+});
